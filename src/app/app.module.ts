469795f4e57504a4134cee18e3365f8de80d3572
Luis Herrera <la.herrera457@gmail.com>
2018-02-27 18:09:36 +0100

    added the guards and the auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './components/index/index.component';
+
+import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+
+import { AuthService } from './services/auth.service';
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
 
 const routes: Routes = [
-  { path: '',  component: IndexComponent },
+  { path: '',  component: IndexPageComponent, canActivate: [ InitAuthGuardService ] },
+  { path: 'login',  component: LoginPageComponent, canActivate: [ RequireAnonGuardService ]},
+  { path: 'signup',  component: SignupPageComponent, canActivate: [ RequireAnonGuardService ]},
+  // { path: 'page',  component: ... , canActivate: [ RequireUserGuardService ] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexPageComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    RequireAnonGuardService,
+    RequireUserGuardService,
+    InitAuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
