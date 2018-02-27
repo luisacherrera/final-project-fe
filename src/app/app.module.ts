161213c9f591caaf7fff480c89bf82857d37a80e
Luis Herrera <la.herrera457@gmail.com>
2018-02-27 16:42:52 +0100

    setting up the front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
 
+const routes: Routes = [
+  { path: '',  component: IndexComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
